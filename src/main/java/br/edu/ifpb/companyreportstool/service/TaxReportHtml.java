873d19e785fe0bd654af3f8d6ca731e66e9c2578
John <ewertonjohn8@gmail.com>
2022-03-21 23:48:20 -0300

    refactor update

---
+++ src/main/java/br/edu/ifpb/companyreportstool/service/TaxReportHtml.java
@@ -0,0 +1,27 @@
+package br.edu.ifpb.companyreportstool.service;
+
+import br.edu.ifpb.companyreportstool.repository.TaxRepository;
+import org.springframework.stereotype.Service;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@Service
+public class TaxReportHtml extends TaxReport{
+    public TaxReportHtml(TaxRepository taxRepository) {
+        super(taxRepository);
+    }
+
+    public ReportExporter getExporter() {
+        return new HtmlExporter();
+    }
+
+    public String writeBody() {
+        return getExporter().exportBody("This is the tax Report", fillBody() );
+    };
+
+    public String fillBody() {
+        return "<ul><li>"+super.taxRepository.findAll().stream()
+                .map(Objects::toString).collect(Collectors.joining("</li><li>"))+"</li></ul>";
+    }
+
+}
---
+++ src/main/java/br/edu/ifpb/companyreportstool/service/TaxReportService.java
@@ -0,0 +1,23 @@
+package br.edu.ifpb.companyreportstool.service;
+
+
+public abstract class TaxReportService {
+
+    public String generateReport() {
+        return writeHeader() + writeBody() + writeFooter();
+    }
+
+    protected String writeHeader() {
+        return getExporter().exportHeader("Company Report");
+    }
+
+    public abstract String writeBody();
+
+    protected String writeFooter() {
+        return getExporter().exportFooter("2022 - Design Patterns IFPB");
+    }
+
+    public abstract ReportExporter getExporter();
+
+
+}
